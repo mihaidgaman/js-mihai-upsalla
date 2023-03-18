c1fc9f0b4c6ba4e71417c75b62a4cbdc0e3251ed
Dragos Iordache <dragosh.iordache@yahoo.com>
2023-03-18 18:27:08 +0200

    added better variable exercise

--- m01s03/ex06/app.js
+++ m01s03/ex06/app.js
@@ -1,6 +1,10 @@
 console.log(`
-    Dragos este instructor de programare. Dragos studiaza JavaScript de la 17 ani. 
-    Cand Dragos a absolvit facultatea si-a facut licenta in JavaScript. 
-    Dragos are si un master in tehnologii internet. 
-    Dragos este instructor Pixellab si preda JavaScript acolo.
+  Acest website („site-ul”) este administrat de catre The JavaScripts SRL,
+  cu sediul social in Strada Scripturilor, Numarul 94, Etaj 1, Sector 1, Bucuresti,
+  inregistrata la Registrul Comertului sub nr. J40/11111/2017,
+  cod unic de inregistrare 12345678 („Pixellab”).
+  Operatorul datelor cu caracter personal este
+  The JavaScripts SRL, prin brand-ul „Pixellab”, cu
+  punctul de lucru la adresa: Strada Scripturilor, Numarul 94, Etaj 1, Sector 1, Bucuresti;
+  In continuare, The JavaScripts SRL va fi numit „Pixellab”.
 `);
