04ef9d476e579a96e229156d6066faaa41762b37
Dragos Iordache <dragosh.iordache@yahoo.com>
2023-03-22 19:34:19 +0200

    removed jquery exercise

--- m02s02/ex01/app.js
+++
@@ -1,6 +0,0 @@
-// var p = document.createElement('p');
-// p.innerText = 'Textul care trebuie sa apara in paragraf';
-// p.id = 'myParagraph';
-// p.classList.add('clase', 'de', 'css');
-
-// document.body.appendChild(p);
